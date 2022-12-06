09430169a091b85af286cb4a23e183b120fcdfee
Hazel <54999364+JiayiGuan99@users.noreply.github.com>
2022-12-06 01:47:55 -0800

    Add files via upload

---
+++ include/RTObj.h
@@ -0,0 +1,14 @@
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include "RTGeometry.h"
+#ifndef __RTOBJ_H__
+#define __RTOBJ_H__
+
+class RTObj : public RTGeometry {
+public:
+
+    void init(const char* filename);
+
+};
+
+#endif 
